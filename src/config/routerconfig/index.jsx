48c2be4812ff84079f6a7e2e9c09e160ebd0a871
Sakiynat10 <xoliyorvasilbek4@gmail.com>
2025-03-07 17:55:55 +0500

    adding admin/hisobotlar

--- src/config/routerconfig/index.jsx
+++ src/config/routerconfig/index.jsx
@@ -35,6 +35,7 @@ import DirectorProduct from '@/pages/Director/pages/Product';
 import DirectorSeller from '@/pages/Director/pages/Seller';
 import DirectorReport from '@/pages/Director/pages/Report';
 import ProductDetails from '@/pages/Director/pages/Product/components/ProductDetalies';
+import ReportProductDetails from './../../pages/Admin/pages/Report/components/ReportProductDetails';
 export default function RouterConfig() {
   return (
     <Routes>
@@ -42,47 +43,78 @@ export default function RouterConfig() {
       <Route path="*" element={<Error404 />} />
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ADMIN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       <Route path="/admin" element={<Admin />}>
-        <Route index element={<Profile />} /> {/* Этот компонент будет отображаться при переходе на /admin */}
-        
+        <Route index element={<Profile />} />{" "}
+        {/* Этот компонент будет отображаться при переходе на /admin */}
         <Route path="admin-panel" element={<AdminAdminPanel />}>
-          <Route path="storege" element={<AdminStorege />} /> {/* /admin/admin-panel/statistics */}
-          <Route path="users" element={<AdminUsers />} /> {/* /admin/admin-panel/users */}
+          <Route path="storege" element={<AdminStorege />} />{" "}
+          {/* /admin/admin-panel/statistics */}
+          <Route path="users" element={<AdminUsers />} />{" "}
+          {/* /admin/admin-panel/users */}
         </Route>
-        <Route path="report" element={<AdminReport />}></Route>
+        <Route path="report" element={<AdminReport />} />
+        <Route path="report/:name" element={<ReportProductDetails />} />
       </Route>
 
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~WAREHOUSE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       <Route path="/warehouse" element={<Warehouse />}>
-        <Route index element={<Profile />} /> {/* Этот компонент будет отображаться при переходе на /warehouse */}
-        <Route path="product-list" element={<WarehouseProducts />} /> {/* /warehouse/products */}
-        <Route path="send-to-showcase" element={<WarehouseSendProductsToShopView />} /> {/* /warehouse/send-to-showcase */}
-        <Route path="remove-from-showcase" element={<WarehouseRemoveProductsFromShowcase />} /> {/* /warehouse/remove-from-showcase */}
-        <Route path="transfer-to-warehouse" element={<WarehouseTransferProductsToWarehouse />} /> {/* /warehouse/transfer-to-warehouse */}
-        <Route path="order-products" element={<WarehouseOrderProducts />} /> {/* /warehouse/order-products */}
-        <Route path="cash-register" element={<WarehouseCashregister />} /> {/* /warehouse/cash */}
-        <Route path="shop" element={<WarehouseShop />}/>
+        <Route index element={<Profile />} />{" "}
+        {/* Этот компонент будет отображаться при переходе на /warehouse */}
+        <Route path="product-list" element={<WarehouseProducts />} />{" "}
+        {/* /warehouse/products */}
+        <Route
+          path="send-to-showcase"
+          element={<WarehouseSendProductsToShopView />}
+        />{" "}
+        {/* /warehouse/send-to-showcase */}
+        <Route
+          path="remove-from-showcase"
+          element={<WarehouseRemoveProductsFromShowcase />}
+        />{" "}
+        {/* /warehouse/remove-from-showcase */}
+        <Route
+          path="transfer-to-warehouse"
+          element={<WarehouseTransferProductsToWarehouse />}
+        />{" "}
+        {/* /warehouse/transfer-to-warehouse */}
+        <Route
+          path="order-products"
+          element={<WarehouseOrderProducts />}
+        />{" "}
+        {/* /warehouse/order-products */}
+        <Route path="cash-register" element={<WarehouseCashregister />} />{" "}
+        {/* /warehouse/cash */}
+        <Route path="shop" element={<WarehouseShop />} />
         <Route path="report" element={<WarehouseReport />}></Route>
         <Route path="shop">
-          <Route path="return-products" element={<WarehouseReturnProducts />} /> {/* /warehouse/shop/return-products */}
+          <Route path="return-products" element={<WarehouseReturnProducts />} />{" "}
+          {/* /warehouse/shop/return-products */}
         </Route>
       </Route>
 
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SELLER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       <Route path="/seller" element={<Seller />}>
-        <Route index element={<Profile />} /> {/* Этот компонент будет отображаться при переходе на /seller */}
-        <Route path="product-list" element={<SellerProduct />} /> {/* /seller/product-list */}
-        <Route path="warehouse" element={<SellerWarehouse />} /> {/* /seller/warehouse */}
-        <Route path="report" element={<SellerReport />} /> {/* /seller/report */}
+        <Route index element={<Profile />} />{" "}
+        {/* Этот компонент будет отображаться при переходе на /seller */}
+        <Route path="product-list" element={<SellerProduct />} />{" "}
+        {/* /seller/product-list */}
+        <Route path="warehouse" element={<SellerWarehouse />} />{" "}
+        {/* /seller/warehouse */}
+        <Route path="report" element={<SellerReport />} />{" "}
+        {/* /seller/report */}
       </Route>
-
 
       {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~DIRECTOR~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
       <Route path="/director" element={<Director />}>
-        <Route index element={<Profile />} /> {/* Этот компонент будет отображаться при переходе на /seller */}
-        <Route path="seller-list" element={<DirectorSeller/>} /> {/* /Director/product-list */}
-        <Route path="product-list" element={<DirectorProduct />} /> {/* /Director/product-list */}
-        <Route path="product-list/:name" element={<ProductDetails />} /> {/* Динамический маршрут */}
-        <Route path="report" element={<DirectorReport />} /> {/* /Director/report */}
+        <Route index element={<Profile />} />{" "}
+        {/* Этот компонент будет отображаться при переходе на /seller */}
+        <Route path="seller-list" element={<DirectorSeller />} />{" "}
+        {/* /Director/product-list */}
+        <Route path="product-list" element={<DirectorProduct />} />{" "}
+        {/* /Director/product-list */}
+        <Route path="product-list/:name" element={<ProductDetails />} />{" "}
+        {/* Динамический маршрут */}
+        <Route path="report" element={<DirectorReport />} />{" "}
+        {/* /Director/report */}
       </Route>
     </Routes>
   );
